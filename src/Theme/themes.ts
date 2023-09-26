75157d5161a7e22fdc06484c26547202b7672446
Nicolas Cava <nmcava01@gmail.com>
2023-09-25 22:23:38 -0400

    first commit

---
+++ src/Theme/themes.ts
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material'; 
+
+
+export const theme = createTheme({
+    typography: {
+        fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif'
+    },
+    palette: {
+        primary: {
+            main: '#0F7196'
+        },
+        secondary: {
+            main: '#022A19'
+        },
+        info : {
+            main: '#A01299'
+        }
+    }
+})
